df46f65f100374c60d8e4b663a626f2619d984d9
Arianna Telesca <arianna.tlsc@gmail.com>
2022-07-21 12:19:04 +0200

    api connection

---
+++ apiConn.php
@@ -0,0 +1,30 @@
+<?php 
+$API_key = 'AIzaSyADr5BLQb1yjMtHftZIhhUEj96FvESVLMM';
+$youtubePL1 = 'PLW_c2xKfxEIqpPCrfw_twlTSWYiiwvnq-';
+$youtubePL2= 'RDqWAqMzB31lQ';
+$maxResults = '15';
+
+$apiError = 'Video not found';
+try{
+  $apiData = @file_get_contents('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C+id&playlistId='.$youtubePL1.'&maxResults='.$maxResults.'&key='.$API_key.'');
+  if($apiData){
+    $videolist = json_decode($apiData);
+  } else {
+    throw new Exception('Invalid API key or channel ID.');
+  }
+} catch(Exception $e){
+    $apiError = $e->getMessage();
+  }
+
+try{
+    $apiDataMusic = @file_get_contents('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2C+id&playlistId='.$youtubePL2.'&maxResults='.$maxResults.'&key='.$API_key.'');
+    if($apiDataMusic){
+      $videolistMusic = json_decode($apiDataMusic);
+    } else {
+      throw new Exception('Invalid API key or channel ID.');
+    }
+  } catch(Exception $e){
+      $apiError = $e->getMessage();
+    }
+
+?>
