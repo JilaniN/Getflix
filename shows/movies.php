83031c9d42fcd0debba29be684bf76e399f325eb
Arianna Telesca <arianna.tlsc@gmail.com>
2022-08-04 11:51:40 +0200

    movies

--- shows/movies.php
+++ shows/movies.php
@@ -1,7 +1,7 @@
 <?php 
 $API_key = 'AIzaSyADr5BLQb1yjMtHftZIhhUEj96FvESVLMM';
-$channelID = 'PL4WiRZw8bmXvAw7LyLC3LIuLDoagogZdb';
-$maxResults = '15';
+$channelID = $_GET['id'];
+$maxResults = '50';
 
 $apiError = 'Video not found';
 try{
@@ -14,6 +14,26 @@ try{
 } catch(Exception $e){
     $apiError = $e->getMessage();
   }
+
+
+  $category = '';
+  
+  if ($channelID == 'PLW_c2xKfxEIqpPCrfw_twlTSWYiiwvnq-'){
+    $category = 'Sport';
+  } 
+  elseif ($channelID == 'RDqWAqMzB31lQ'){
+    $category = 'Music';
+  }
+  elseif ($channelID == 'PL4WiRZw8bmXvAw7LyLC3LIuLDoagogZdb'){
+    $category = 'Cooking';
+  }
+  elseif ($channelID == 'PLriZt3RmcI30iIudgKFINROyCK2Jmo4Z_'){
+    $category = 'Movies';
+  }
+  elseif ($channelID == 'PLW_c2xKfxEIoV9Udl7Q9wzikc3P28d1X7'){
+    $category = 'Games';
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -34,7 +54,7 @@ try{
     <!-- link icon in head -->
     <link rel="apple-touch-icon" type="image/png" sizes="16x16" href="./assets/ventilateur.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./assets/ventilateur.png">
-    <title>Movies</title>
+    <title><?php echo $category;?></title>
 </head>
 <body>
 
@@ -68,17 +88,22 @@ try{
 </div>
 
 <div  class="container">
-  <h1 class="head">Videos</h1>
+<div class='containervideo'>
+<div class="row justify-content-around">
+
   <?php
   if(!empty($videolist->items)){
     foreach($videolist->items as $item){
-      if(isset($item->id->videoid)){
+      if(isset($item->snippet->resourceId->videoId)){
         ?>
-        <div class="yvideo-box">
-          <iframe width="280" height="150" src="https://www.youtube.com/embed/<?php echo $item->id->videoid; ?>" frameborder="0"  allowfullscreen></iframe>
-            <h4><?php echo $item->snippet->title; ?></h4>
+        
+        <div class="card m-5" style="width: 20rem;">
+        <img width="85%" src="https://img.youtube.com/vi/<?php echo $item->snippet->resourceId->videoId; ?>/maxresdefault.jpg" class="card-img-top" alt=""></img>
+        <div class="card-body">
+        <h5 class="card-title"><?php echo $item->snippet->title; ?></h5>
         </div>
-        <?php
+        </div>
+       <?php
       }
     }
   } else{
@@ -86,6 +111,26 @@ try{
   }
   ?>
 </div>
+</div>
+</div>
 
+<footer class="footer p-2 pt-3 mt-5">
+  <div class="footer-cols">
+    <ul>
+      <li><a href="./faq.php">FAQ</a></li>
+    </ul>
+    <ul>
+      <li><a href="./contact.php">Contact Us</a></li>
+    </ul>
+    <ul>
+    <li><a href="./auth/home.php">BesToBe Originals</a></li>
+    </ul>
+    <ul>
+      <li><a href="#">Copyright 2022 BesTOBe</a></li>
+   </ul>
+  </div>
+</footer>
+<!-- link script js -->
+<script src="myscript.js"></script>
 </body>
 </html>
