234a3ed341c5cd08414bc5386e4328fd869ab210
Sophie Cheng <chengsophie25@gmail.com>
2022-08-07 16:40:48 +0200

    button colour

--- forget_passwood_management/forget.php
+++ forget_passwood_management/forget.php
@@ -11,8 +11,10 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
   <!-- link css same as home -->
-  <link rel="stylesheet" href="../signin/home.css">
-  <link rel="stylesheet" href="../signin/sign.css">
+  <!-- <link rel="stylesheet" href="../signin/home.css"> -->
+  <!-- <link rel="stylesheet" href="../signin/sign.css"> -->
+  <link rel="stylesheet" href="../signin/home.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="../signin/sign.css?v=<?php echo time(); ?>">
   <script src="https://kit.fontawesome.com/6c36406174.js" crossorigin="anonymous"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -50,7 +52,7 @@
                   </div>
                   <!-- Submit button -->
                   <div class="text-center">
-                  <button type="submit" class="btnlogin btn btn-success btn-block mb-4" name ="forget-btn">Receive new password by e-mail</button>
+                  <button type="submit" class="btnlogin btn btn-danger btn-block mb-4" name ="forget-btn">Receive new password by e-mail</button>
                   </div>
 </form>
 <?php
